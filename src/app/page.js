c69877279ce800d5dfa389b7343d9184da3fafcd
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-04-15 15:53:34 +0500

    second secrtion

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import HomeServices from "@/components/home/HomeServices";
+
 export default function Home() {
   return (
-    <div className="text-primary font-['Chakra'] bg-black h-screen font-bold text-6xl">
-    
+    <div className="text-primary font-['Chakra'] bg-black h- font-bold text-6xl">
+      <HomeServices/>
     </div>
   );
 }
--- src/components/Home/section2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function section2() {
-  return (
-    <div>section2</div>
-  )
-}
-
-export default section2
