a042abddca06b96e8ad1827c63f166a2d246e1ad
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2025-04-16 12:07:35 +0500

    some changes in hero header for responsive in xl 2xl lg md

--- src/components/common/button.jsx
+++ src/components/common/button.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Button = ({ text, link, className }) => {
     return (
         <Link href={link}>
-            <button className={ ` ${className} text-[16px] rounded-[8px] align-middle  cursor-pointer`}>
+            <button className={ ` ${className} text-[12px] xl:text-[16px] rounded-[8px] align-middle  cursor-pointer`}>
                 {text}
             </button>
         </Link >
