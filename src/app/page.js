ca4e7eb2063801a55d811140b719d84ebea47089
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2025-04-16 15:46:02 +0500

    chnages

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,18 @@ import CaseStudies from "@/components/home/case-studies";
 import CentralizeIntelligence from "@/components/home/centralize-intelligence";
 import Hero from "@/components/Home/hero";
 import HomeServices from "@/components/home/home-services";
+import HouseAIProduct from "@/components/Home/house-ai-product";
 import { cardData } from "@/data/home-blog";
 
 export default function Home() {
   return (
-    <div className="bg-black">
+    <div className="">
       <Hero />
       <HomeServices />
       <CentralizeIntelligence />
       <HouseAIProduct />
       <CaseStudies />
       <Blog heading='Discover Our Blog Articles' para="Explore our latest posts for insights on design, branding, and innovation. Stay updated with fresh ideas and trends in the creative world" cardData={cardData} />
-      import HouseAIProduct from "@/components/home/house-ai-product"
     </div>
   );
 }
