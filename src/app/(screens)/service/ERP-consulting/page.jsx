7345c1efc0eae11d541babe52885da1f1737d606
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-24 15:04:25 +0500

    Update

--- src/app/(screens)/service/ERP-consulting/page.jsx
+++ src/app/(screens)/service/ERP-consulting/page.jsx
@@ -1,10 +1,14 @@
 import BrandSlider from '@/components/common/brand-slider';
 import { ERPSolutionsBottomSection } from '@/components/common/solutions';
-import Service_Inner_Hero from '@/components/service/ERP-Cosultant/hero';
+import { Blog } from '@/components/homepage/blog';
+import Testimonials from '@/components/homepage/testimonial';
 import ERPServicesEast from '@/components/service/ERP-Cosultant/ERP-services-east';
-import ERP_Consultant_Hero from '@/components/service/ERP-Cosultant/hero';
+import Service_Inner_Hero from '@/components/service/ERP-Cosultant/hero';
 import { service_brand_icons } from '@/data/brand-slider-icons-data';
 import { blackSection, blackSection2, bottomSection, bottomSection2 } from '@/data/erp-consulting-service';
+import { cardData } from '@/data/home-blog';
+import { testimonials } from '@/data/home-testimonials';
+import ArticleSlider from "@/components/common/article-slider";
 
 const ERPConsulting = () => {
     return (
@@ -18,9 +22,12 @@ const ERPConsulting = () => {
                     text={"Trusted by world's most exciting brands"}
                     brand_icons={service_brand_icons} />
             </div>
+            <ERPServicesEast />
             <ERPSolutionsBottomSection className='flex justify-center items-center gap-5' bottomSection={bottomSection} left={true} blackSection={blackSection} />
             <ERPSolutionsBottomSection className='flex flex-row-reverse justify-center items-center gap-20' blackSection={blackSection2} bottomSection={bottomSection2} left={false} />
-            <ERPServicesEast/>
+            <Testimonials testimonials={testimonials} />
+            <ArticleSlider />
+            <Blog heading='Discover Our Blog Articles' para="Explore our latest posts for insights on design, branding, and innovation. Stay updated with fresh ideas and trends in the creative world" cardData={cardData} />
         </div>
     )
 }
