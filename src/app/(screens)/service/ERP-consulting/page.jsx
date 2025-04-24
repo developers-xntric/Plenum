16f1c5c9302f1e1f7f1a92358cdd43727ccf8536
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-24 14:12:22 +0500

    Service ERP Consulting Hero

--- src/app/(screens)/service/ERP-consulting/page.jsx
+++ src/app/(screens)/service/ERP-consulting/page.jsx
@@ -1,4 +1,6 @@
+import BrandSlider from '@/components/common/brand-slider';
 import Service_Inner_Hero from '@/components/service/ERP-Cosultant/hero';
+import { service_brand_icons } from '@/data/brand-slider-icons-data';
 
 const ERPConsulting = () => {
     return (
@@ -7,11 +9,11 @@ const ERPConsulting = () => {
                 heading={"Plenum Tech offers top ERP solutions across Dubai and the Middle East"}
                 para={"Delivering tailored IT services for business growth and operational excellence."}
             />
-            {/* <div className='2xl:max-w-[1300px] h-full w-[90%] mx-auto space-y-20 py-16'>
+            <div className='2xl:max-w-[1300px] h-full w-[90%] mx-auto space-y-20 py-16'>
                 <BrandSlider
                     text={"Trusted by world's most exciting brands"}
                     brand_icons={service_brand_icons} />
-            </div> */}
+            </div>
         </div>
     )
 }
--- src/components/service/ERP-Cosultant/hero.jsx
+++ src/components/service/ERP-Cosultant/hero.jsx
@@ -1,16 +1,21 @@
 import Button from '@/components/common/button';
+import Image from 'next/image';
 import React from 'react';
 
 const Service_Inner_Hero = ({ heading, para }) => {
   return (
-    <div className='md:h-screen'>
-
+    <div className='md:h-screen 2xl:h-auto overflow-hidden'>
       {/* Wrapper */}
-      <div className='2xl:max-w-[1300px] h-full w-[90%] mx-auto space-y-20 py-16 md:py-28'>
-        <div className='w-[350px] h-[420px] space-y-8'>
-          <h1 className='text-secondary font-bold text-[50px] font-["Chakra"] leading-13'>{heading}</h1>
-          <p className='text-secondary font-normal text-[18px]'>{para}</p>
-          <Button text={"Let's Talk"} link={"/contact"} className={"px-10 py-2 bg-primary text-white"} />
+      <div className='2xl:max-w-[1300px] h-full w-[90%] mx-auto my-auto space-y-20 py-16 md:py-28'>
+        <div className='flex items-center w-full justify-between'>
+          <div className='w-[377px] space-y-8 mt-12'>
+            <h1 className='text-secondary font-bold md:text-[35px] xl:text-[48px] font-["Chakra"] md:leading-10 xl:leading-[52px]'>{heading}</h1>
+            <p className='text-secondary font-normal md:text-[14px] xl:text-[18px] opacity-60'>{para}</p>
+            <Button text={"Let's Talk"} link={"/contact"} className={"px-10 py-2 bg-primary text-white"} />
+          </div>
+          <div className='md:w-[600px] md:h-[400px] xl:w-[813px] xl:h-[663px] relative left-[5.55%]'>
+            <Image src={"/service/ERPC/ERPC-Hero.svg"} alt='ERPC' width={1500} height={1500} />
+          </div>
         </div>
       </div>
     </div>
