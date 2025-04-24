0d850b88f4a8edebee56b56f76358d525482ecc9
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-24 13:40:21 +0500

    Commit:

--- src/app/(screens)/service/ERP-consulting/page.jsx
+++ src/app/(screens)/service/ERP-consulting/page.jsx
@@ -1,12 +1,12 @@
-import BrandSlider from '@/components/common/brand-slider';
-import ERP_Consultant_Hero from '@/components/service/ERP-Cosultant/hero';
-import { service_brand_icons } from '@/data/brand-slider-icons-data';
-import React from 'react';
+import Service_Inner_Hero from '@/components/service/ERP-Cosultant/hero';
 
 const ERPConsulting = () => {
     return (
         <div>
-            <ERP_Consultant_Hero />
+            <Service_Inner_Hero
+                heading={"Plenum Tech offers top ERP solutions across Dubai and the Middle East"}
+                para={"Delivering tailored IT services for business growth and operational excellence."}
+            />
             {/* <div className='2xl:max-w-[1300px] h-full w-[90%] mx-auto space-y-20 py-16'>
                 <BrandSlider
                     text={"Trusted by world's most exciting brands"}
--- src/components/service/ERP-Cosultant/hero.jsx
+++ src/components/service/ERP-Cosultant/hero.jsx
@@ -1,11 +1,20 @@
+import Button from '@/components/common/button';
 import React from 'react';
 
-const ERP_Consultant_Hero = () => {
+const Service_Inner_Hero = ({ heading, para }) => {
   return (
-    <div className='md:h-screen bg-cover bg-no-repeat overflow-x-hidden'>
-      <div className='2xl:max-w-[1300px] h-full w-[90%] mx-auto space-y-20 py-16'></div>
+    <div className='md:h-screen'>
+
+      {/* Wrapper */}
+      <div className='2xl:max-w-[1300px] h-full w-[90%] mx-auto space-y-20 py-16 md:py-28'>
+        <div className='w-[350px] h-[420px] space-y-8'>
+          <h1 className='text-secondary font-bold text-[50px] font-["Chakra"] leading-13'>{heading}</h1>
+          <p className='text-secondary font-normal text-[18px]'>{para}</p>
+          <Button text={"Let's Talk"} link={"/contact"} className={"px-10 py-2 bg-primary text-white"} />
+        </div>
+      </div>
     </div>
   )
 }
 
-export default ERP_Consultant_Hero;
+export default Service_Inner_Hero;
