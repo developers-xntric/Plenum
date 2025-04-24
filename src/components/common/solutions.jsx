27fcca4beea87da70fcb60f76ec4055a547bf0b9
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2025-04-24 15:48:48 +0500

    changes in service section

--- src/components/common/solutions.jsx
+++ src/components/common/solutions.jsx
@@ -6,9 +6,9 @@ import Image from 'next/image'
 const ERPSolutions = ({ blackSection }) => {
     return (
         <section className='relative font-["Archivo"] bg-[#282526]'>
-            <div className='max-w-[85%] mx-auto text-center py-16  '>
+            <div className='max-w-[85%] xl:max-w-[80%] mx-auto text-center py-16  '>
                 <h2 className='text-[50px] text-white'>{blackSection.heading}</h2>
-                <p className='text-[18px] text-[#FFF] mt-6'>{blackSection.para}</p>
+                <p className='text-[18px] text-[#FFF] mt-6 opacity-60'>{blackSection.para}</p>
                 <Button text={blackSection.button} className='bg-transparent text-white text-[15px] mt-8 border border-[#FFFFFF] px-9 py-2' link='/' />
             </div>
         </section>
@@ -22,17 +22,17 @@ export const ERPSolutionsBottomSection = ({ bottomSection, className, left, blac
             <div className={`max-w-[95%] 2xl:max-w-[1300px] ${left ? "ms-auto" : ""}  `}>
                 <div className={`${className}`}>
                     <div className='w-[50%]'>
-                        <h2 className='text-[39px] '>{bottomSection.heading}</h2>
+                        <h2 className='text-[39px] text-secondary font-medium home-section-headings'>{bottomSection.heading}</h2>
                         {
                             bottomSection.Input.map((item, index) => {
                                 return (
                                     <div key={index} className='flex items-center gap-2 mt-8'>
-                                        <input type="input" placeholder={item} className='w-[80%] py-2 border-b border-[#BABABA]' />
+                                        <input type="input" placeholder={item} className='w-[80%] py-2 border-b border-[#BABABA] text-secondary font-medium home-section-headings px-2 placeholder:text-secondary placeholder:font-medium placeholder:home-section-headings' />
                                     </div>
                                 )
                             })
                         }
-                        <Button text={bottomSection.button} className='bg-transparent  text-[15px] mt-8
+                        <Button text={bottomSection.button} className='bg-transparent cursor-pointer  text-[15px] mt-8
                         border border-[#282526] px-9 py-2' link='/' />
                     </div>
                     <div className='w-[50%] flex justify-center items-center'>
