bc3c033825ee7041e56a9ac20ab6be43777ffc9a
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-24 16:10:03 +0500

    Blog Article Adjustment

--- src/app/(screens)/service/ERP-consulting/page.jsx
+++ src/app/(screens)/service/ERP-consulting/page.jsx
@@ -33,11 +33,6 @@ const ERPConsulting = () => {
             </div>
             <Testimonials testimonials={testimonials} />
             <ArticleSlider />
-            <Blog
-                heading='Discover Our Blog Articles'
-                para="Explore our latest posts for insights on design, branding, and innovation. Stay updated with fresh ideas and trends in the creative world"
-                cardData={cardData}
-            />
             <Blog heading='Discover Our Blog' para="Explore our latest posts for insights on design, branding, and innovation. Stay updated with fresh ideas and trends in the creative world" cardData={cardData} />
         </div>
     )
