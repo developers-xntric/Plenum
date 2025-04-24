c9e9f9e3c81ae8e3c84bbc8c5302440fd03b9222
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-24 13:25:18 +0500

    Updation

---
+++ src/components/service/ERP-Cosultant/hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ERP_Consultant_Hero = () => {
+  return (
+    <div className='md:h-screen bg-cover bg-no-repeat overflow-x-hidden'>
+      <div className='2xl:max-w-[1300px] h-full w-[90%] mx-auto space-y-20 py-16'></div>
+    </div>
+  )
+}
+
+export default ERP_Consultant_Hero;
