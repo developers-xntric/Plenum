9970479314ef4b657121e5f15e3394cbfec24682
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-25 14:57:11 +0500

    Section3 FAO

--- src/app/(screens)/service/finance-opearations/page.jsx
+++ src/app/(screens)/service/finance-opearations/page.jsx
@@ -1,4 +1,7 @@
+import { ERPSolutionsBottomSection } from '@/components/common/solutions';
+import FaoDynamicOpearation from '@/components/service/finance-and-opearations/fao-dynamic-opearation';
 import New_Service_Inner_Hero from '@/components/service/finance-and-opearations/hero';
+import { fao_service, fao_service_black } from '@/data/erp-consulting-service';
 
 const FinanceOpearations = () => {
     return (
@@ -10,6 +13,10 @@ const FinanceOpearations = () => {
                 image_className="md:w-[500px] md:h-[300px] xl:w-[650px] xl:h-[500px]"
                 button_text="Connect with our Experts"
             />
+            <div className='max-w-full 2xl:max-w-[1440px] mx-auto '>
+                <ERPSolutionsBottomSection className='flex justify-center items-center gap-5' bottomSection={fao_service} left={true} blackSection={fao_service_black} />
+            </div>
+            <FaoDynamicOpearation />
         </div>
     )
 }
--- src/components/common/solutions.jsx
+++ src/components/common/solutions.jsx
@@ -7,7 +7,7 @@ const ERPSolutions = ({ blackSection }) => {
     return (
         <section className='relative font-["Archivo"] bg-[#282526]'>
             <div className='max-w-[90%] xl:max-w-[75%] 2xl:max-w-[55%]  mx-auto text-center py-16  '>
-                <h2 className='text-[50px] text-white'>{blackSection.heading}</h2>
+                <h2 className='text-[50px] text-white w-[90%] mx-auto'>{blackSection.heading}</h2>
                 <p className='text-[18px] text-[#FFF] mt-6 opacity-60'>{blackSection.para}</p>
                 <Button text={blackSection.button} className='bg-transparent text-white text-[15px] mt-8 border border-[#FFFFFF] px-9 py-2' link='/' />
             </div>
@@ -22,7 +22,7 @@ export const ERPSolutionsBottomSection = ({ bottomSection, className, left, blac
             <div className={`max-w-[95%] 2xl:max-w-[1610px] ${left ? "ms-auto" : ""}  `}>
                 <div className={`${className}`}>
                     <div className='w-[50%] 2xl:w-[60%]'>
-                        <h2 className='text-[39px] text-secondary font-medium home-section-headings'>{bottomSection.heading}</h2>
+                        <h2 className='text-[39px] text-secondary font-medium home-section-headings w-[60%]'>{bottomSection.heading}</h2>
                         {
                             bottomSection.Input.map((item, index) => {
                                 return (
