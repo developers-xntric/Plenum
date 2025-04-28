8c07b2e19b729a3aa3a104de04ff2bcf0bbe2cfd
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-04-28 10:45:28 +0500

    testimonial arrow button updated

--- src/components/homepage/testimonial.jsx
+++ src/components/homepage/testimonial.jsx
@@ -22,11 +22,11 @@ export default function Testimonials({ testimonials }) {
           <div className="flex  justify-end items-center mb-4">
             <div className="flex absolute gap-4 left-[95%] mb-10">
               <div className="flex items-center gap-1">
-                <CarouselPrevious className="h-10 w-fit hover:bg-transparent border-none cursor-pointer" />
+                <CarouselPrevious className="h-10 w-fit hover:bg-transparent border-none cursor-pointer shadow-none" />
                 {/* <p className="relative -left-3 text-secondary font-medium home-section-headings">Prev</p> */}
               </div>
               <div className="flex items-center">
-                <CarouselNext className="h-10 w-fit hover:bg-transparent border-none cursor-pointer relative left-4" />
+                <CarouselNext className="h-10 w-fit hover:bg-transparent border-none cursor-pointer relative left-4 shadow-none" />
                 {/* <p className="relative right-13 text-secondary font-medium home-section-headings">Next</p> */}
               </div>
             </div>
