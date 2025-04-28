931165c3b6a3910a43214f1fdd9d605e4dbb9949
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-28 13:20:04 +0500

    Updation

---
+++ src/components/product/momentum-ai/hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MomentumAIHero = () => {
+    return (
+        <div className='h-screen bg-cover' style={{
+            backgroundImage: "url('/product/m_hero.svg')"
+        }}>
+            <div className='2xl:max-w-[1440px] w-[90%] mx-auto space-y-20 py-16 min-h-screen'>
+            </div>
+        </div>
+    )
+}
+
+export default MomentumAIHero
