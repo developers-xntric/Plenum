da17d4ae435579afa5f2f97019c9aec4244383bf
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-29 15:52:50 +0500

    Industry Done

--- src/components/common/case-hero.jsx
+++ src/components/common/case-hero.jsx
@@ -4,9 +4,9 @@ const CaseHero = ({ heading, para }) => {
     return (
         <section className='relative pt-44'>
             <div className='flex justify-center items-center'>
-                <div className='max-w-[50%] 2xl:max-w-[30%] mx-auto'>
-                    <h1 className='text-[60px] font-["Chakra"] font-medium leading-[65px] text-center tracking-[-2.771px] text-[#FF6035]'>{heading || "Fueling Innovation with AI-Powered Possibilities"}</h1>
-                    <p className='text-center mt-4 text-[#151515] opacity-60'>{para || "We create AI tools that amplify creativity and solve real-world problems. From concept to impact, our technology powers the next wave of innovation."}</p>
+                <div className='max-w-[45%] 2xl:max-w-[30%] mx-auto'>
+                    <h1 className='text-[60px] font-["Chakra"] font-semibold leading-[65px] text-center tracking-[-2.771px] text-[#FF6035]'>{heading || "Fueling Innovation with AI-Powered Possibilities"}</h1>
+                    <p className='text-center text-[18px] mt-4 text-[#151515] opacity-60'>{para || "We create AI tools that amplify creativity and solve real-world problems. From concept to impact, our technology powers the next wave of innovation."}</p>
                 </div>
             </div>
         </section>
