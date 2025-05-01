f5e8d6e67d04061d2422f6a9f99763b97078f660
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-05-01 13:33:10 +0500

    Page Case Study Details

--- src/app/(screens)/case-studies/[id]/page.jsx
+++ src/app/(screens)/case-studies/[id]/page.jsx
@@ -1,43 +1,47 @@
 import Image from "next/image"
 import Section2 from "@/components/case-studies/section2"
+import CaseDetailHero from "@/components/case-studies/case-details-hero"
+import Personalization from "@/components/case-studies/personalization"
+import AISolutions from "@/components/product/AI-solutions"
+import TeamTestimonials from "@/components/careers/team-testimonials"
+import IndustryShowcase from "@/components/industries/industry-showcase"
 
 export default function BrandOSPage() {
   return (
-    <main className="min-h-screen 2xl:max-w-[1440px] mx-auto ">
-      {/* Hero Section */}
-      <div className="relative h-screen w-full overflow-hidden">
-        <div className="absolute inset-0 z-10"></div>
-        <Image
-          src="/casestudy/casestudy-hero1.png"
-          alt="Dynamic starburst background"
-          fill
-          className="object-cover"
-          priority
-        />
-
-        <div className="absolute inset-0 z-20 my-20">
-
-          <div className="container mx-auto px-4 pt-32 md:pt-40">
-          <div className="mb-6">
-              <button className="px-8 py-3 text-white text-lg bg-white/20 hover:bg-white/30 border border-white/70 rounded-full transition-colors font-[Archivo] font-medium ">
-                Healthcare
-              </button>
+    <main>
+      <CaseDetailHero />
+      <Section2 />
+      <Personalization />
+      <div className="py-16">
+        <AISolutions
+          title={"Create Comprehensive AI Solutions Without Coding"}
+          paragraph={"Accelerate digital transformation with Momentum AI by PlenumTech — a no-code platform that streamlines data ingestion, ML model training, deployment, and workflow automation."}
+          image={'/casestudy/cs4.svg'} />
+      </div>
+      <div className="2xl:max-w-[1440px] lg:w-[82%] xl:w-[65%] 2xl:w-[50%] mx-auto py-20">
+        <div className="w-[700px] mx-auto flex flex-col gap-10">
+          <div className="w-full h-[70px] flex justify-center">
+            <Image src={"/images/brand-logos/momentum.webp"} alt="Logo" width={1500} height={1500} className="w-[196.083px]" />
+          </div>
+          <p className="text-center text-[20px] font-['Archivo'] font-normal opacity-60">“The talent bar at Plenum is extremely high, and there is something electric and deeply fulfilling about working with a team that is so passionate about our mission and what we do.”</p>
+          <div className="flex items-center justify-center">
+            <div className="w-10 h-10 rounded-full overflow-hidden mr-3 bg-gray-200">
+              <Image
+                src={"/careers/p1.svg"}
+                alt={"People"}
+                width={40}
+                height={40}
+                className="object-cover"
+              />
             </div>
-
-            <h1 className="text-4xl md:text-5xl lg:text-6xl  text-white mb-4 max-w-3xl font-bold font-['Chakra] ">
-              Turn soft solutions into real results
-            </h1>
-
-            <p className="text-white/90 max-w-2xl text-lg md:text-xl  font-bold font-['Chakra] ">
-              At Plenum Technologies, we offer cutting-edge AI software platforms tailored to empower data engineers,
-              scientists, analysts, and automation engineers.
-            </p>
+            <div>
+              <p className="font-medium text-[16px]">Maroun Najjar</p>
+              <p className="text-xs text-gray-600">Head of Design</p>
+            </div>
           </div>
         </div>
       </div>
-      {/* Section 2  */}
-     <Section2/>
-     
+      <IndustryShowcase />
     </main>
   )
 }
---
+++ src/components/case-studies/case-details-hero.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import React from 'react'
+
+const CaseDetailHero = () => {
+    return (
+        <div className='bg-cover bg-no-repeat' style={{ backgroundImage: "url('/casestudy/casestudy-hero1.png')" }}>
+            <div className="2xl:max-w-[1440px] w-[85%] mx-auto space-y-20 py-16 xl:py-24 h-screen 2xl:h-[900px]">
+
+                <div className="container mx-auto px-4 pt-32 md:pt-40">
+                    <div className="mb-6">
+                        <button className="px-6 py-1 text-white text-lg bg-[rgba(114, 114, 114, 0.50)] hover:bg-white/30 border border-white/70 rounded-full transition-colors font-[Archivo] font-medium ">
+                            Healthcare
+                        </button>
+                    </div>
+
+                    <h1 className="text-4xl md:text-5xl lg:text-6xl text-white mb-4 max-w-xl font-semibold font-['Chakra'] ">
+                        Turn soft solutions into real results
+                    </h1>
+
+                    <p className="text-white opacity-60 max-w-[480px] text-lg md:text-xl font-normal font-['Archivo'] ">
+                        At Plenum Technologies, we offer cutting-edge AI software platforms tailored to empower data engineers,
+                        scientists, analysts, and automation engineers.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CaseDetailHero
