9528740d3440d6a402d672b53cc8cbc459624544
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-05-02 10:24:17 +0500

    Updation

--- src/components/homepage/house-ai-product.jsx
+++ src/components/homepage/house-ai-product.jsx
@@ -39,38 +39,6 @@ export default function HouseAIProduct() {
             <div className="text-white p-7 pb-0 xl:h-80 rounded-2xl space-y-6 relative z-50">
               <div className='space-y-2'>
                 <div className="flex justify-between items-start ">
-                  <h3 className="text-3xl font-medium font-['Archivo']">Momentum AI</h3>
-                    <motion.div
-                      whileHover="hover"
-                      className="text-primary flex items-center gap-2 underline underline-offset-4 cursor-pointer z-40"
-                    >
-                      View Product
-                      <motion.svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="11"
-                        height="11"
-                        viewBox="0 0 11 11"
-                        fill="none"
-                        className={"mt-1"}
-                        variants={{
-                          hover: {
-                            x: [0, 3, 0],
-                            y: [0, -3, 0],
-                            transition: {
-                              duration: 0.8,
-                              repeat: Infinity,
-                              ease: "easeInOut"
-                            }
-                          }
-                        }}
-                      >
-                        <path
-                          d="M0.9375 10.214L9.67107 1.48047M9.67107 1.48047H0.9375M9.67107 1.48047V10.214"
-                          stroke="#FF6035"
-                          strokeWidth="1.74671"
-                        />
-                      </motion.svg>
-                    </motion.div>
                   <h3 className="text-[22px] md:text-3xl font-medium font-['Archivo']">Momentum AI</h3>
                   <Link href="#" className="text-primary flex items-center gap-2 underline underline-offset-4 cursor-pointer z-40 text-[11.279px] md:text-[14px] my-auto">View Product
                     <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
@@ -114,38 +82,6 @@ export default function HouseAIProduct() {
             <div className="text-white p-7 h-full rounded-2xl shadow-xl space-y-6  relative z-50">
               <div className='space-y-2'>
                 <div className="flex justify-between items-start">
-                  <h3 className="text-3xl font-medium font-['Archivo']">Vertical builds</h3>
-                    <motion.div
-                      whileHover="hover"
-                      className="text-primary flex items-center gap-2 underline underline-offset-4 cursor-pointer z-40"
-                    >
-                      View Product
-                      <motion.svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="11"
-                        height="11"
-                        viewBox="0 0 11 11"
-                        fill="none"
-                        className={"mt-1"}
-                        variants={{
-                          hover: {
-                            x: [0, 3, 0],
-                            y: [0, -3, 0],
-                            transition: {
-                              duration: 0.8,
-                              repeat: Infinity,
-                              ease: "easeInOut"
-                            }
-                          }
-                        }}
-                      >
-                        <path
-                          d="M0.9375 10.214L9.67107 1.48047M9.67107 1.48047H0.9375M9.67107 1.48047V10.214"
-                          stroke="#FF6035"
-                          strokeWidth="1.74671"
-                        />
-                      </motion.svg>
-                    </motion.div>
                   <h3 className="text-[22px] md:text-3xl font-medium font-['Archivo']">Vertical builds</h3>
                   <Link href="#" className="text-primary flex items-center gap-2 underline underline-offset-4 cursor-pointer z-40 text-[11.279px] md:text-[14px] my-auto">View Product
                     <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
