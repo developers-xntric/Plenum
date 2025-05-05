cee7539be554a82bbb42e710e7f0b418708af064
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-05-05 10:46:16 +0500

    Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/layout/navbar";
 import "./globals.css";
 import Footer from "../components/layout/footer";
+import MobileNav from "@/components/layout/mobile-nav";
 
 export const metadata = {
   title: "Plenum",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
         cz-shortcut-listen="true"
       >
         <Navbar />
+        <MobileNav />
         {children}
         <Footer />
       </body>
