71ef947881ebdb356fa2017b538109ec88666f04
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-05-05 11:40:59 +0500

    Service Page Responsive

--- src/components/service/hero.jsx
+++ src/components/service/hero.jsx
@@ -2,32 +2,32 @@ import Image from 'next/image'
 import React from 'react'
 import Button from '../common/button'
 
-const ServiceHero = ({ bgImage, isLogo=true}) => {
+const ServiceHero = ({ bgImage, isLogo = true }) => {
   return (
-    <div className='md:h-screen bg-cover bg-no-repeat bg-center overflow-x-hidden' style={{ backgroundImage: `url(${bgImage})` }}>
+    <div className='h-screen bg-cover bg-no-repeat bg-center overflow-x-hidden' style={{ backgroundImage: `url(${bgImage})` }}>
       {/* Wrapper */}
       <div className="2xl:max-w-[1440px] h-full w-[90%] mx-auto space-y-20 py-16 xl:pt-36">
 
         {/* Image Div */}
-       {isLogo && <div className='xl:w-[200px] xl:h-[250px] w-[130px] h-[180px] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+        {isLogo && <div className='xl:w-[200px] xl:h-[250px] w-[130px] h-[180px] absolute left-1/2 lg:top-1/2 md:top-[40%] top-[25%] transform -translate-x-1/2 -translate-y-1/2'>
           <Image src="/service/sp_hero.svg" alt='Plenum AI Logo' width={1500} height={1500} className='w-full h-full object-contain' />
         </div>}
 
-        <div className='w-full h-full flex mt-10 xl:mt-0'>
+        <div className='w-full h-full flex lg:flex-row flex-col lg:mt-10 xl:mt-0 lg:gap-0 gap-8'>
           {/* Centered Text Below Image */}
-          <div className="flex flex-col justify-end h-full items-start w-[530px]">
-            <h1 className='md:text-[60px] xl:text-[90px] font-["Chakra"] font-[600] text-primary flex flex-col gap-2 md:leading-12 xl:leading-20'>Plenum <span>AI Services</span></h1>
-            <p className='mt-8 md:text-[14px] xl:text-[18px] font-normal text-gray-400 w-[450px]'>
+          <div className="flex flex-col justify-end h-full items-start lg:w-[530px]">
+            <h1 className='text-[44px] lg:text-[60px] xl:text-[90px] font-["Chakra"] font-[600] text-primary flex flex-col gap-2 leading-10 lg:leading-12 xl:leading-20'>Plenum <span>AI Services</span></h1>
+            <p className='mt-8 text-[17px] lg:text-[14px] xl:text-[18px] font-normal text-gray-400 lg:w-[450px]'>
               At Plenum Technologies, we offer cutting-edge AI software platforms tailored to empower data engineers, scientists, analysts, and automation engineers.
             </p>
           </div>
           {/* Right-Aligned Text and Button */}
-          <div className="text-gray-400 md:w-[340px] xl:w-96 ms-auto">
-            <p className='mb-8 md:text-[14px] xl:text-[18px] font-normal'>
+          <div className="text-gray-400 lg:w-[340px] xl:w-96 lg:ms-auto">
+            <p className='mb-8 text-[17px] lg:text-[14px] xl:text-[18px] font-normal'>
               Our flagship solution, Momentum AI, is designed to simplify and supercharge your AI development process with minimal or no coding —accelerating innovation across industries.
             </p>
             {/* Button */}
-            <Button className={"px-8 py-2.5 hover:bg-[#FF6035] hover:transition-colors ease-in-out duration-300 hover:text-white  tracking-tighter font-semibold bg-[#FFF] text-black rounded"} text={"Request a Demo"} link={"/service"} />
+            <Button className={"px-8 py-2.5 hover:bg-[#FF6035] hover:transition-colors ease-in-out duration-300 hover:text-white  tracking-tighter font-semibold bg-[#FFF] text-black rounded font-['Archivo'] text-[15px]"} text={"Request a Demo"} link={"/service"} />
           </div>
         </div>
       </div>
