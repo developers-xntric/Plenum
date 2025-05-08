59d338e94bd765232173ca86b781850d73a5cfa0
manal-xntric <developers@xntric.me>
2025-05-08 17:41:34 +0500

    Create page.jsx

---
+++ src/app/(screens)/case-studies/retail-erp/page.jsx
@@ -0,0 +1,21 @@
+import Section2 from "@/components/case-studies/section2"
+import CaseDetailHero from "@/components/case-studies/case-details-hero"
+import Personalization from "@/components/case-studies/personalization"
+import { testimonials } from "@/data/home-testimonials"
+import Testimonials from "@/components/homepage/testimonial"
+import ArticleSlider from "@/components/common/article-slider"
+import { Blog } from "@/components/homepage/blog"
+import { cardData } from "@/data/home-blog"
+
+export default function BrandOSPage() {
+  return (
+    <main>
+      <CaseDetailHero />
+      <Section2 />
+      <Personalization />
+      <Testimonials testimonials={testimonials} />
+      <ArticleSlider />
+      <Blog heading='Discover Our Blog Articles' para="Explore our latest posts for insights on design, branding, and innovation. Stay updated with fresh ideas and trends in the creative world" cardData={cardData} />
+    </main>
+  )
+}
