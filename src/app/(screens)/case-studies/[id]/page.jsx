d821dfe7886dd581da8b5d86113f467d219ff44f
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-05-08 16:49:32 +0500

    Case Study Service

--- src/app/(screens)/case-studies/[id]/page.jsx
+++ src/app/(screens)/case-studies/[id]/page.jsx
@@ -6,6 +6,8 @@ import Testimonials from "@/components/homepage/testimonial"
 import ArticleSlider from "@/components/common/article-slider"
 import { Blog } from "@/components/homepage/blog"
 import { cardData } from "@/data/home-blog"
+import { ServiceList } from "@/data/service-list"
+import CaseStudyList from "@/components/case-studies/case-studies-list"
 
 export default function BrandOSPage() {
   return (
@@ -13,6 +15,7 @@ export default function BrandOSPage() {
       <CaseDetailHero />
       <Section2 />
       <Personalization />
+      <CaseStudyList serviceList={ServiceList} />
       <Testimonials testimonials={testimonials} />
       <ArticleSlider />
       <Blog heading='Discover Our Blog Articles' para="Explore our latest posts for insights on design, branding, and innovation. Stay updated with fresh ideas and trends in the creative world" cardData={cardData} />
---
+++ src/components/case-studies/case-studies-list.jsx
@@ -0,0 +1,75 @@
+"use client"
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { useState } from "react";
+
+const servicesData = [
+    {
+        id: 1,
+        title: "ERP Consulting & Implementation",
+    },
+    {
+        id: 2,
+        title: "AI Consulting, Governance & Implementation",
+    },
+    {
+        id: 3,
+        title: "Momentum",
+    },
+    {
+        id: 4,
+        title: "Managed Cloud Services",
+    },
+];
+export default function HomeServices() {
+    const [openAccordion, setOpenAccordion] = useState(null);
+
+    const toggleAccordion = (id) => {
+        setOpenAccordion(openAccordion === id ? null : id);
+    };
+
+    return (
+        <div className="bg-white">
+            {/* Main content */}
+            <div className="2xl:max-w-[1440px] w-[90%] mx-auto space-y-10 md:space-y-20 py-8 md:py-16 xl:py-24">
+
+                <div className="flex flex-col space-y-10 sm:space-y-16">
+
+                    {/* BOTTOM column - Services */}
+                    <div className="flex md:flex-row flex-col items-start justify-between xl:max-w-[90%]">
+                        {/* LEFT */}
+                        <div className="space-y-2 md:space-y-6 w-full md:w-[80%]">
+                            <div className="relative font-medium">
+                                <h2 className="text-[30px] lg:text-[50px] xl:text-[57.375px] font-semibold text-secondary opacity-[0.8] inline-block md:block font-['Archivo'] tracking-tighter">
+                                    Technologies
+                                </h2>
+                                <h2 className="text-[30px] lg:text-[50px] xl:text-[57.375px] font-semibold text-secondary opacity-[0.8] inline-block lg:block md:mx-0 mx-2 font-['Archivo'] tracking-tighter mt-[-32px]">
+                                    Implemented
+                                </h2>
+                            </div>
+                        </div>
+
+                        {/* RIGHT - Accordion */}
+                        <div className="w-full md:w-[65%] space-y-0 md:my-0 my-14 font-['Archivo']">
+                            {servicesData.map((item) => (
+                                <div
+                                    key={item.id}
+                                    className="border-t-[2.5px] border-[#e7e7e7] py-5"
+                                >
+                                    <div
+                                        onClick={() => toggleAccordion(item.id)}
+                                        className="group flex items-center gap-3 font-medium text-secondary lg:text-[21px] cursor-pointer"
+                                    >
+                                        <p className="group-hover:text-[#FF6035] leading font-bold transition-colors w-[90%] leading-[-0.485px]  duration-500 font-['Archivo']">
+                                            {item.title}
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
