45eb398912946c1f2a014488354609db5c6daf43
manal-xntric <developers@xntric.me>
2025-05-09 18:38:13 +0500

    pushed

--- src/data/home-testimonials.js
+++ src/data/home-testimonials.js
@@ -2,33 +2,33 @@ export const testimonials = [
   {
     id: 1,
     quote:
-      "“Plenum’s approach to design and innovation has transformed how we operate. Their team consistently delivers exceptional results.”",
-    author: "Sarah Johnson",
-    position: "Director of Operations",
+      "Plenum showcases deep knowledge and a thorough understanding of every project they undertake. Their expertise allows them to anticipate challenges and provide effective solutions, ensuring smooth progress. This strong foundation, combined with clear communication, enables them to deliver high-quality results well ahead of schedule.",
+    author: "Khalid Abi Kahlil",
+    position: "CTOO SCB Bahrain",
     socialLink: "https://linkedin.com",
   },
   {
     id: 2,
     quote:
-      "“Working with Plenum has been a game-changer for our business. Their insight-driven approach has helped us reach new heights.”",
-    author: "Michael Chen",
-    position: "Chief Marketing Officer",
+      "Plenum is one of the most professional and reliable companies. They are fully transparent throughout every project and consistently deliver well ahead of schedule. Their commitment to clear communication and timely completion truly sets them apart",
+    author: "Shehrazade Hasan",
+    position: "CTOO SCB Qatar ",
     socialLink: "https://linkedin.com",
   },
   {
     id: 3,
     quote:
-      "“Plenum’s approach to design and innovation has transformed how we operate. Their team consistently delivers exceptional results.”",
-    author: "Sarah Johnson",
-    position: "Director of Operations",
+      "Plenum showcases deep knowledge and a thorough understanding of every project they undertake. Their expertise allows them to anticipate challenges and provide effective solutions, ensuring smooth progress. This strong foundation, combined with clear communication, enables them to deliver high-quality results well ahead of schedule.",
+    author: "Khalid Abi Kahlil",
+    position: "CTOO SCB Bahrain",
     socialLink: "https://linkedin.com",
   },
   {
     id: 4,
     quote:
-      "“Working with Plenum has been a game-changer for our business. Their insight-driven approach has helped us reach new heights.”",
-    author: "Michael Chen",
-    position: "Chief Marketing Officer",
+      "Plenum is one of the most professional and reliable companies. They are fully transparent throughout every project and consistently deliver well ahead of schedule. Their commitment to clear communication and timely completion truly sets them apart",
+    author: "Shehrazade Hasan",
+    position: "CTOO SCB Qatar ",
     socialLink: "https://linkedin.com",
   },
 ]
