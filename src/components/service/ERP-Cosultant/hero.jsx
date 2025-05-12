25575b1d6ecf6497e4c9d34b7e77dee0aa890842
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-05-12 13:05:53 +0500

    changes

--- src/components/service/ERP-Cosultant/hero.jsx
+++ src/components/service/ERP-Cosultant/hero.jsx
@@ -30,7 +30,7 @@ const Service_Inner_Hero = ({ heading, para, is_space_in_image = true, image, im
       <div className='2xl:max-w-[1440px] lg:w-[90%] mx-auto my-auto space-y-20 pt-10 md:py-16 lg:py-28'>
         <div className='flex lg:flex-row flex-col items-center md:items-start lg:items-center w-full h-full justify-between gap-8 lg:gap-0 mt-12 lg:mt-0'>
           {/* Left Section Heading Text And Buttons */}
-          <div className={`lg:w-[45%] text-center md:text-start space-y-6 md:space-y-8 mt-12 ms-0 md:ms-20 lg:ms-0 ${width < 350 && "px-5"}`}>
+          <div className={`lg:w-[45%] text-center md:text-start space-y-6 md:space-y-8 mt-12 ms-0 md:ms-20 lg:ms-0 ${width < 425 && "px-5"}`}>
             {/* Heading */}
             <h1 className={`text-secondary ${width < 350 && "text-[38px]"} text-[39px] lg:text-[50px] 2xl:text-[54px] font-["Chakra"] leading-[42px] md:leading-[55px] font-semibold home-section-headings ${heading_className}`}>{heading}</h1>
             {/* Paragraph */}
