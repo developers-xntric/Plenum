c146563a311f4c50ce05b98830583d8bb1ce9716
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-05-12 13:44:47 +0500

    testimonial data

--- src/data/home-testimonials.js
+++ src/data/home-testimonials.js
@@ -18,17 +18,17 @@ export const testimonials = [
   {
     id: 3,
     quote:
-      "Plenum showcases deep knowledge and a thorough understanding of every project they undertake. Their expertise allows them to anticipate challenges and provide effective solutions, ensuring smooth progress. This strong foundation, combined with clear communication, enables them to deliver high-quality results well ahead of schedule.",
-    author: "Khalid Abi Kahlil",
-    position: "CTOO SCB Bahrain",
+      "“Plenum’s approach to design and innovation has transformed how we operate. Their team consistently delivers exceptional results.”",
+    author: "Balasubramanian Rajendran",
+    position: "Lead - AI",
     socialLink: "https://linkedin.com",
   },
   {
     id: 4,
     quote:
-      "Plenum is one of the most professional and reliable companies. They are fully transparent throughout every project and consistently deliver well ahead of schedule. Their commitment to clear communication and timely completion truly sets them apart",
-    author: "Shehrazade Hasan",
-    position: "CTOO SCB Qatar ",
+      "“Working with Plenum has been a game-changer for our business. Their insight-driven approach has helped us reach new heights.”",
+    author: "Shashank Kanangi",
+    position: "Data Scientist ",
     socialLink: "https://linkedin.com",
   },
 ]
