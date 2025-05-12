866c42a06b318a29f9b2a8cae12a9518fbbffa3d
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-05-12 20:31:45 +0500

    hero

--- src/components/homepage/hero.jsx
+++ src/components/homepage/hero.jsx
@@ -65,7 +65,7 @@ const Hero = () => {
             {/* //hero section main heading */}
             <div className='flex w-full md:w-[90%] lg:w-[85%] xl:w-[80%] 2xl:w-[1200px] mx-auto justify-center h-[80vh] md:h-screen mt-32 md:mt-40'>
                 <div className='w-full mx-auto flex flex-col gap-4'>
-                    <h1 className='max-w-[98%] md:max-w-[90%] mx-auto text-white 2xl:text-[75px] text-[32px] sm:text-[55px] font-semibold xl:text-[70px] leading-[36px] md:leading-[50px] xl:leading-[71px] 2xl:leading-[80px] font-["Chakra"] text-center'>
+                    <h1 className='max-w-[98%] md:max-w-[90%] lg:max-w-[800px] xl:max-w-[90%] mx-auto text-white 2xl:text-[75px] text-[32px] sm:text-[55px] font-semibold xl:text-[70px] leading-[36px] md:leading-[50px] xl:leading-[71px] 2xl:leading-[80px] font-["Chakra"] text-center'>
                         Empower your<span className='text-primary'> enterprise</span> to think smarter, operate faster, and grow intelligently
 
                     </h1>
