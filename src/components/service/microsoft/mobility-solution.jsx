132c37edee931b21d5a0cbbaa3bf3afe0e265ddc
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-05-14 16:02:11 +0500

    Mobility Solutions

--- src/components/service/microsoft/mobility-solution.jsx
+++ src/components/service/microsoft/mobility-solution.jsx
@@ -143,7 +143,7 @@ export default function MobilitySolutions({
                 height={40}
                 className="w-10 h-10"
               />
-              <h3 className="text-[20px] font-semibold text-sceondary">{solution.title}</h3>
+              <h3 className="text-[20px] font-semibold text-sceondary lg:min-h-12">{solution.title}</h3>
               <p className="text-xs 2xl:text-base text-[#4D4D56]">
                 {solution.description || "Solution is easy to use and totally customizable to your business needs."}
               </p>
