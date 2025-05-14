bc5784a75553afff5eebfe7265d98f78c95d1573
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-05-14 15:44:50 +0500

    Updated

--- src/components/service/ERP-Cosultant/Tabs-images.jsx
+++ src/components/service/ERP-Cosultant/Tabs-images.jsx
@@ -70,59 +70,41 @@ export default function TabImageGallery({ activeState }) {
 
           <div className="bg-primary font-['Archivo'] text-white p-3 md:p-5 absolute flex flex-col justify-start items-start left-0 bottom-0 w-[400px]">
             <div className="flex items-center gap-3">
-              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="33" viewBox="0 0 26 33" fill="none">
-                <g>
-                  <path
-                    d="M25.5 13.5337C25.5 19.4535 16.6256 28.552 13.8317 31.2697C13.3657 31.7231 12.6343 31.7231 12.1683 31.2697C9.3744 28.552 0.5 19.4535 0.5 13.5337C0.5 6.50799 6.09644 0.8125 13 0.8125C19.9036 0.8125 25.5 6.50799 25.5 13.5337Z"
-                    fill="white"
-                    fillOpacity="0.2"
-                  />
-                  <path
-                    d="M13 0.65625C19.9924 0.65625 25.6562 6.42473 25.6562 13.5342C25.6561 15.0496 25.0892 16.7543 24.209 18.4912C23.3275 20.2305 22.125 22.016 20.8379 23.6973C18.2633 27.0603 15.3396 30.0208 13.9404 31.3818C13.4138 31.894 12.5862 31.894 12.0596 31.3818C10.6604 30.0208 7.73665 27.0603 5.16211 23.6973C3.87504 22.016 2.67247 20.2305 1.79102 18.4912C0.910803 16.7543 0.34386 15.0496 0.34375 13.5342C0.34375 6.42473 6.00757 0.65625 13 0.65625Z"
-                    stroke="url(#paint0_linear_2402_5245)"
-                    strokeOpacity="0.5"
-                    strokeWidth="0.3125"
-                  />
+              <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="5" y="30.7676" width="30" height="4.54545" rx="2.27273" fill="white" />
+                <foreignObject x="0.9375" y="-3.75" width="38.125" height="43.9219"><div xmlns="http://www.w3.org/1999/xhtml" style={{ "backdrop- filter": "blur(3.12px);clip-path:url(#bgblur_0_2402_5242_clip_path);height:100%;width:100%" }}></div></foreignObject><g data-figma-bg-blur-radius="6.25">
+                  <path d="M32.5 15.5337C32.5 21.4535 23.6256 30.552 20.8317 33.2697C20.3657 33.7231 19.6343 33.7231 19.1683 33.2697C16.3744 30.552 7.5 21.4535 7.5 15.5337C7.5 8.50799 13.0964 2.8125 20 2.8125C26.9036 2.8125 32.5 8.50799 32.5 15.5337Z" fill="white" fill-opacity="0.2" />
+                  <path d="M20 2.65625C26.9924 2.65625 32.6562 8.42474 32.6562 15.5342C32.6561 17.0496 32.0892 18.7543 31.209 20.4912C30.3275 22.2305 29.125 24.016 27.8379 25.6973C25.5852 28.64 23.0653 31.2745 21.5332 32.7988L20.9404 33.3818C20.4466 33.8621 19.688 33.8921 19.1611 33.4717L19.0596 33.3818C17.6604 32.0208 14.7367 29.0603 12.1621 25.6973C10.875 24.016 9.67247 22.2305 8.79102 20.4912C7.9108 18.7543 7.34386 17.0496 7.34375 15.5342C7.34375 8.42474 13.0076 2.65625 20 2.65625Z" stroke="url(#paint0_linear_2402_5242)" stroke-opacity="0.5" stroke-width="0.3125" />
                 </g>
-                <g filter="url(#filter1_d_2402_5245)">
-                  <circle cx="13" cy="12.6875" r="3.75" fill="#D9D9D9" />
+                <g filter="url(#filter1_d_2402_5242)">
+                  <circle cx="20" cy="14.6875" r="3.75" fill="#D9D9D9" />
                 </g>
                 <defs>
-                  <filter id="filter1_d_2402_5245" x="8" y="8.9375" width="10" height="10" filterUnits="userSpaceOnUse">
-                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                    <feColorMatrix
-                      in="SourceAlpha"
-                      type="matrix"
-                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                      result="hardAlpha"
-                    />
+                  <clipPath id="bgblur_0_2402_5242_clip_path" transform="translate(-0.9375 3.75)"><path d="M32.5 15.5337C32.5 21.4535 23.6256 30.552 20.8317 33.2697C20.3657 33.7231 19.6343 33.7231 19.1683 33.2697C16.3744 30.552 7.5 21.4535 7.5 15.5337C7.5 8.50799 13.0964 2.8125 20 2.8125C26.9036 2.8125 32.5 8.50799 32.5 15.5337Z" />
+                  </clipPath><filter id="filter1_d_2402_5242" x="15" y="10.9375" width="10" height="10" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
                     <feOffset dy="1.25" />
                     <feGaussianBlur stdDeviation="0.625" />
                     <feComposite in2="hardAlpha" operator="out" />
                     <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
-                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
-                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2402_5242" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2402_5242" result="shape" />
                   </filter>
-                  <linearGradient
-                    id="paint0_linear_2402_5245"
-                    x1="0.5"
-                    y1="7.75136"
-                    x2="23.1982"
-                    y2="43.2909"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stopOpacity="0" />
-                    <stop offset="1" stopColor="white" />
+                  <linearGradient id="paint0_linear_2402_5242" x1="7.5" y1="9.75136" x2="30.1982" y2="45.2909" gradientUnits="userSpaceOnUse">
+                    <stop stop-opacity="0" />
+                    <stop offset="1" stop-color="white" />
                   </linearGradient>
                 </defs>
               </svg>
 
-              <h2 className="text-[18px] md:text-[30px] font-semibold">{activeTab}</h2>
+
+              <h2 className="text-[18px] md:text-[30px] font-semibold">{activeTab === "UAE" ? "United Arab Emirates" : activeTab}</h2>
             </div>
             <p className="text-[11px] md:text-base text-left max-w-[70%] md:max-w-[95%] mt-3">{tabData[activeTab].address}</p>
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
