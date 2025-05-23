d3d79d7a7b792e23a0e065e6051603ff6dc84604
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-05-23 16:57:15 +0500

    image removed on about

--- src/components/about/our-people.jsx
+++ src/components/about/our-people.jsx
@@ -12,12 +12,6 @@ export default function OurPeopleSection() {
       postion: "Head Of Government Partnerships & Special Projects"
     },
     {
-      id: 2,
-      image: "/about/P2.png",
-      name: "Sarvesh Gaokar",
-      postion: "Marketing Manager"
-    },
-    {
       id: 3,
       image: "/about/P4.png",
       name: "Sammas Sulaiman",
@@ -32,7 +26,7 @@ export default function OurPeopleSection() {
   ])
 
   return (
-    <section className="py-16  mx-auto font-['Archivo']">
+    <section className="py-16 2xl:py-20 mx-auto font-['Archivo']">
       <div className="text-center mb-16">
         <h2 className="text-4xl md:text-5xl lg:text-6xl mb-6 text-[#101010]">Our People</h2>
         <p className="text-secondary opacity-[0.6] font-medium max-w-3xl mx-auto text-[15px] md:text-lg px-3 md:px-0">
@@ -41,26 +35,26 @@ export default function OurPeopleSection() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 w-[90%] mx-auto 2xl:max-w-[1440px] gap-4 lg:grid-cols-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 gap-8 w-[90%] mx-auto 2xl:max-w-[1440px]">
         {teamMembers.map((member) => (
           <div
             key={member.id}
-            className="group bg-white rounded-lg overflow-hidden transition-all duration-300"
+            className="group b rounded-lg overflow-hidden transition-all duration-300 "
           >
             <div className="relative aspect-[3/4] w-full overflow-hidden">
               <Image
                 src={member.image || "/placeholder.svg"}
-                alt="Team member"
+                alt={member.name}
                 fill
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 60vw, 350vw"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
                 className="object-cover object-center transition-transform duration-500 group-hover:scale-105"
                 priority={member.id <= 2}
                 quality={90}
               />
             </div>
-            <div className="flex mt-4 justify-start md:text-left text-center flex-col gap-1 font-['Archivo'] pb-4 md:pb-0">
+            <div className="flex mt-4 justify-start md:text-left text-center flex-col gap-1 font-['Archivo'] px-4 pb-4 md:pb-0">
               <h4 className="text-[22px] text-[#000000] font-medium">{member.name}</h4>
-              <p className="text-[17px] md:max-w-[90%] text-[#000000B2] font-medium">{member.postion}</p>
+              <p className="text-[17px] text-[#000000B2] font-medium">{member.postion}</p>
             </div>
           </div>
         ))}
