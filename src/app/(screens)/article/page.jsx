94826bb5e15c7122e8a940f4a9d84d0da9ab974d
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2025-05-27 18:59:53 +0500

    sad

--- src/app/(screens)/article/page.jsx
+++ src/app/(screens)/article/page.jsx
@@ -3,8 +3,8 @@ import Banner from '@/components/common/banner'
 import React from 'react'
 
 export const metadata = {
-  title: 'About Plenum | AI Innovators Powering Digital Change',
-  description: 'Learn about Plenum’s mission, vision, and leadership in AI-powered innovation and enterprise technology solutions.',
+  title: 'Plenum Articles | Expert Takes on Tech & Trends ',
+  description: 'Explore in-depth articles on AI, cloud, and digital innovation curated by Plenum tech experts and industry professionals.',
   alternates: {
     canonical: 'https://plenum-tech.com/article',
   },
