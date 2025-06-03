0570a7dfa77063e97027cd0b3599a821a8d3567a
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2025-06-03 18:58:03 +0500

    asd

---
+++ src/app/(screens)/blog/common-types-of-erp/page.jsx
@@ -0,0 +1,204 @@
+import { Blog } from "@/components/homepage/blog";
+import { cardData } from "@/data/home-blog";
+import Image from "next/image";
+export const metadata = {
+  title: 'Dynamics vs NetSuite: Comparing Microsoft and Oracle ERP Solutions',
+  description: "Discover the key differences between Microsoft Dynamics vs NetSuite. Explore features, pricing, scalability, and industry fit to choose the best ERP for your business.",
+  alternates: {
+    canonical: 'https://plenum-tech.com/blog/dynamics-netsuite',
+  },
+};
+
+
+export default function CommonTypes() {
+  return (
+    <>
+      <div className=" font-['Archivo'] pt-34 lg:pt-52">
+
+        {/* HEADING AND DATE */}
+        <div className="2xl:max-w-[1440px] w-[90%] mx-auto">
+          <div className="lg:max-w-[72%] 2xl:max-w-[60%] py-10">
+            <p className="text-[#636363] text-[15px] lg:text-[17px] font-['Archivo'] font-medium">May 26 2025</p>
+            <h2 className="text-secondary font['Archivo'] leading-[34px] lg:leading-[56px] text-[30px] lg:text-[46px] font-semibold">Common Types of ERP: Understanding the Options Available</h2>
+          </div>
+        </div>
+
+        {/* IMAGE */}
+        <Image src="/blog/Article1.jpg" alt="Blog Image" width={1000} height={1000} className="w-full h-60 md:h-full" />
+
+        {/* CONTENT */}
+        <div className="2xl:max-w-[1440px] w-[90%] mx-auto flex items-center justify-end font-['Archivo']">
+          <div className="md:px-6 pb-2 md:pb-12 py-12  space-y-12 lg:max-w-[80%] xl:max-w-[70%] 2xl:max-w-[65%]">
+            <p className="text-[15px] lg:text-lg text-[#6D6E76] font-medium font-['Archivo']">
+              Essential tools for controlling and integrating company operations are Enterprise Resource Planning (ERP) systems. From supply chains and customer relationship management to finance and HR, ERP systems consolidate and automate procedures thereby producing a single source of truth. Not all ERP systems, however, are the same. Selecting the appropriate one mostly relies on knowledge of the many ERP options on the market nowadays.
+            </p>
+            <p className="text-[15px] lg:text-lg text-[#6D6E76] font-medium font-['Archivo']">
+              This article will lead you through the many kinds of ERP, their differences, and how to choose which is best fit for your company. This page has you covered whether your quest is for ERP systems, ERP tools, or knowledge on which of the following kinds of ERP will fit your circumstances.
+            </p>
+
+            <div className="space-y-8">
+              <h2 className="text-[25px] lg:text-[36px] font-medium max-w-[80%] 2xl:max-w-[60%] leading-[35px] lg:leading-[42px] font-['Archivo']">What Are the Common Types of ERP? </h2>
+              <div className="space-y-5">
+                <p className="text-[17px] lg:text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  ERP systems may be arranged according to industry emphasis, company size, deployment style, or degree of customizing. On-site ERP, cloud-based ERP, and hybrid ERP are the three most usually known forms of ERP. These reflect the fundamental designs for ERP program development and delivery to companies.
+                </p>
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Knowing these categories will enable companies to assess which system best fits their strategic objectives, technical capacity, and financial situation.</p>
+              </div>
+            </div>
+
+            <div className="space-y-8">
+              <h2 className="text-[25px] lg:text-[36px] font-medium leading-[35px] lg:leading-[42px] font-['Archivo']">On-Premise ERP Systems </h2>
+              <div className="space-y-5">
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  ERP implementation historically comes in on-site form. Under this approach, the program is housed locally on the corporate servers and hardware. This kind of ERP system gives complete control over the security, data, and infrastructure. Often selected by big companies with sophisticated processes and specialized IT teams, it is very flexible.
+                </p>
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  On-site ERP solutions do, however, have large upfront hardware and software expenditures. Long-term expenses may also be added to maintenance, security updates, and system improvements that companies handle. Even with their complexity, on-site solutions are favored by businesses needing strong data management and working in sectors with tight regulatory norms.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[25px] lg:text-[20px] font-medium leading-[35px] lg:leading-[42px] font-['Archivo']">
+                Cloud-Based ERP Systems
+              </h2>
+            </div>
+            <div>
+              <h2 className="text-[25px] lg:text-[36px] font-medium mb-6 leading-[35px] lg:leading-[42px] font-['Archivo']">
+                Cloud-Based Accessibility and Global Collaboration
+              </h2>
+              <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                Modern companies now mostly use cloud-based ERP because of its scalability and adaptability. Under this arrangement, the ERP program is housed on vendor servers and accessible online. Businesses pay a monthly or yearly cost depending on consumption, features, or user count when they register to the service.
+              </p>
+              <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                Since cloud ERP solutions save physical infrastructure investment, they lower upfront expenses. The service provider handles backup, security patch, and update requests. For small to mid-sized firms or those trying to grow rapidly without assembling an in-house IT staff, this makes cloud ERP perfect.
+              </p>
+              <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                Remote work settings and mobile access are also supported by Cloud ERP, therefore allowing teams to remain linked from anywhere. Leading examples include SAP Business ByDesign, Microsoft Dynamics 365, and NetSuite.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-[25px] lg:text-[36px] font-medium mb-6 leading-[35px] lg:leading-[42px] font-['Archivo']">
+                Hybrid ERP Systems
+              </h2>
+              <div className="space-y-5">
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Combining aspects of on-site and cloud ERP paradigms, hybrid ERP systems create This arrangement lets businesses employ cloud-based modules for other activities while keeping basic capabilities in-house. For security, a company may retain its financial information on-site, for instance, and use cloud ERP for customer relationship management or human resources.
+                </p>
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  For companies not yet ready to totally shift to the cloud, hybrid ERP solutions provide flexibility and a transitional road. They keep control over private data while nevertheless offering the advantages of scalability and accessibility. Managing integration between cloud and on-site components may be difficult, however, and can need outside tools or experts.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[25px] lg:text-[36px] font-medium mb-6 leading-[35px] lg:leading-[42px] font-['Archivo']">
+                Types of ERP Software by Business Size
+              </h2>
+              <div className="space-y-5">
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  ERP systems may also be categorized mostly by the size of the company they are meant to service. ERP systems fall generally into small company ERP, mid-market ERP, and enterprise-grade ERP.
+                </p>
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Usually light-weight, small company ERP systems include necessary accounting, invoicing, and inventory control features. Many times cloud-based and with simple setup and low IT participation, these solutions are Two such are Zoho ERP and QuickBooks Online.
+                </p>
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Built for expanding businesses needing more than just the essentials, mid-market ERP systems Deeper capabilities like extensive reporting, multi-currency compatibility, and third-party app connectivity abound from these solutions. Common names in this group include Acumatica and Sage Intacct.
+                </p>
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Strong platforms meant for big companies with many divisions and worldwide operations are enterprise-grade ERP systems. These systems handle thousands of users, provide data analytics, and allow great customizing. Among them are well-known products in this area SAP S/4HANA, Oracle ERP Cloud, and Microsoft Dynamics 365 Finance.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[25px] lg:text-[36px] font-medium mb-6 leading-[35px] lg:leading-[42px] font-['Archivo']">
+                Industry-Specific ERP Systems
+              </h2>
+              <div className="space-y-5">
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Certain ERP systems are designed especially for certain sectors. Features, processes, and compliance tools fit for the particular requirements of the industry are pre-configured in these systems. This cuts the requirement for customizing and shortens installation time.
+                </p>
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Manufacturing ERP systems, for instance, have features for supply chain management, shop floor control, and production planning. ERP for healthcare emphasizes patient data, billing compliance, and healthcare laws. ERP for construction helps control materials planning, subcontractors, and work costs. Food and beverage ERP include monitoring of expiry dates, traceability, and inventory management.
+                </p>
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Selecting an ERP system tailored to a particular sector guarantees that the program fits daily operating needs from the starting point. Businesses must be sure, nevertheless, that the vendor supports upgrades for evolving industry norms and laws.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[25px] lg:text-[36px] font-medium mb-6 leading-[35px] lg:leading-[42px] font-['Archivo']">
+                Open-Source vs. Proprietary ERP Systems
+              </h2>
+              <div className="space-y-5">
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  One further classification for ERP systems is open-source versus proprietary. Source code access provided by open-source ERP systems lets companies personalize the platform to their own requirements. Though companies may pay for creation, maintenance, and support, these systems are frequently free to use. Famously open-source ERP systems include Odoo and ERPNext.
+                </p>
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Conversely, proprietary ERP systems are created by businesses and cannot be changed in the source code. Professional support, frequent upgrades, and a strong feature set abound from these systems. Although they charge license fees, proprietary ERP providers usually provide end-to-end support and improved connection with other corporate technologies.
+                </p>
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Companies with special processes or specialty needs might find great advantage in open-source ERP's flexibility. Those giving stability, support, and compliance first priority sometimes choose proprietary solutions.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[25px] lg:text-[36px] font-medium mb-6 leading-[35px] lg:leading-[42px] font-['Archivo']">
+                Two-Tier ERP Strategy
+              </h2>
+              <div className="space-y-5">
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  A two-tier ERP approach—using separate ERP systems at the corporate and subsidiary levels—is used by certain big companies. While letting subsidiaries utilize more agile systems like NetSuite or Acumatica, a worldwide company may operate SAP or Oracle ERP at headquarters.
+
+                  This strategy strikes a compromise between the need for consistent worldwide reporting and the freedom local business units have to run free. While guaranteeing that data can still roll up to corporate systems for unified reporting, a two-tier ERP system lowers costs and deployment time.
+
+                  To prevent data silos and inconsistencies, this kind of deployment does, however, depend on appropriate integration and governance rules.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[25px] lg:text-[20px] font-medium leading-[35px] lg:leading-[42px] font-['Archivo']">
+                Types of ERP Systems in the Market Today
+              </h2>
+              <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                The ERP software market continues to evolve, offering various types of ERP systems that cater to different industries, budgets, and operational models. Today, businesses can choose from:
+              </p>
+              <ul className="list-disc pl-5 space-y-2 mt-2">
+                <li className="text-[16px] lg:text-[18px] font-medium leading-[35px] lg:leading-[42px] font-['Archivo']">Modular ERP systems, which allow companies to pick only the features they need.  </li>
+                <li className="text-[16px] lg:text-[18px] font-medium leading-[35px] lg:leading-[42px] font-['Archivo']">AI-powered ERP systems, which use artificial intelligence to automate processes and generate predictive insights. </li>
+                <li className="text-[16px] lg:text-[18px] font-medium leading-[35px] lg:leading-[42px] font-['Archivo']">Mobile ERP, which enables employees to access ERP features through smartphones and tablets.  </li>
+                <li className="text-[16px] lg:text-[18px] font-medium leading-[35px] lg:leading-[42px] font-['Archivo']">Vertical ERP solutions, which serve specific industries such as retail, automotive, or logistics.</li>
+                <li className="text-[16px] lg:text-[18px] font-medium leading-[35px] lg:leading-[42px] font-['Archivo']">Global ERP systems, which support multi-language, multi-currency, and international compliance standards.</li>
+              </ul>
+              <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                As technology advances, ERP systems are also incorporating tools like machine learning, voice-enabled assistants, and robotic process automation to deliver more value.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-[25px] lg:text-[36px] font-medium mb-6 leading-[35px] lg:leading-[42px] font-['Archivo']">
+                Which of the Following Types of ERP is Right for Your Business?
+              </h2>
+              <div className="space-y-5">
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Selecting the correct ERP system calls for weighing several elements. Think about your industry, company size, growth path, IT capacity, and budget. On-site ERP might be the best solution if your business requires utmost control and customizing. Cloud ERP is probably the best choice if you give flexibility, cost savings, and simplicity of usage top priorities.
+                  Consider long-term objectives as well. Is worldwide expansion of your company planned? Will you need to add later additional features? An ERP system must be flexible and able to meet evolving demands. Making a wise choice will depend on reviewing case studies, talking with ERP providers, and evaluating prototype versions.
+                </p>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-[25px] lg:text-[36px] font-medium mb-6 leading-[35px] lg:leading-[42px] font-['Archivo']">
+                Conclusion
+              </h2>
+              <div className="space-y-5">
+                <p className="text-lg text-[#6D6E76] font-medium font-['Archivo']">
+                  Businesses that want to simplify processes, enhance decision-making, and attain sustainable development all depend on an awareness of the many forms of ERP. Investing time to learn your alternatives will pay off in the long run whether your demands match any of the following kinds of ERP systems, you are investigating the kinds of ERP programs available, or you are attempting to find which of these forms of ERP fits your need.
+                  Your operations will center on the ERP system you decide upon. Thus, choosing the correct type—cloud, on-site, hybrid, industry-specific, or open-source—is a strategic as well as a technological one.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Blog heading='Discover More Blogs' para="Stay updated with our ongoing blogs inclusive of tips, case study examples and expert views around the usage of AI, cloud services and ERP solutions to support business growth and operational effectiveness." cardData={cardData} />
+      </div>
+    </>
+  );
+}
