39d51f1b45e419661105a73fd23c8e75e80d283a
manal-xntric <developers@xntric.me>
2025-06-17 13:11:16 +0500

    pushwd

--- src/app/(screens)/blog/what-is-microsoft-dynamics-365/page.jsx
+++ src/app/(screens)/blog/what-is-microsoft-dynamics-365/page.jsx
@@ -1,12 +1,55 @@
 import { Blog } from "@/components/homepage/blog";
 import { cardData } from "@/data/home-blog";
 import Image from "next/image";
+
+
+const faqSchema = {
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "mainEntity": [
+    {
+      "@type": "Question",
+      "name": "What is Microsoft Dynamics 365 used for?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Microsoft Dynamics 365 is used to manage business processes across sales, marketing, finance, supply chain, and customer service, combining ERP and CRM capabilities in a unified cloud platform."
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Is Dynamics 365 an ERP or CRM?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Dynamics 365 is both an ERP and CRM platform. It offers separate applications for enterprise resource planning and customer relationship management, integrated within a single ecosystem."
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "What is the function of Dynamics 365?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Its primary function is to streamline operations, improve customer engagement, and provide data-driven insights by integrating various business processes in a connected platform."
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "What is the difference between Microsoft 365 and Dynamics 365?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Microsoft 365 includes productivity tools like Word, Excel, and Outlook, while Dynamics 365 is a business management solution focused on ERP and CRM functionalities."
+      }
+    }
+  ]
+}
 export const metadata = {
     title: 'What is Microsoft Dynamics 365? A Complete Guide',
     description: "Discover Microsoft Dynamics 365 — a cloud-based CRM and ERP platform that streamlines business operations, boosts productivity, and enhances customer engagement. ",
     alternates: {
         canonical: 'https://plenum-tech.com/blog/what-is-microsoft-dynamics-365',
     },
+    other:{
+           "script:ld+json": JSON.stringify(faqSchema),
+    }
 };
 
 
