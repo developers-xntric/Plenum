3ef79217aca912b8fc237706ccd38627723f9832
manal-xntric <developers@xntric.me>
2025-07-19 21:40:16 +0500

    update

--- src/components/homepage/home-services.jsx
+++ src/components/homepage/home-services.jsx
@@ -3,7 +3,6 @@
 import { motion } from 'framer-motion';
 import Link from "next/link";
 import { useState } from 'react';
-
 import Button from "../common/button";
 import Image from "next/image";
 
